62242527a79d0e5dfb87207096a57fec11a35c57
Lawcito <121973056+Lawcito@users.noreply.github.com>
2024-11-23 12:32:28 -0300

    players style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import logo from "../assets/icono2.png";
 function Home() {
   return (
     <div>
+      {/* Hero */}
       <div className="hero h-screen w-screen relative z-10">
         <div
           className="absolute inset-0 h-full"
@@ -19,18 +20,47 @@ function Home() {
         ></div>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
+          {/* Logo */}
           <div className="max-w-md">
             <div className="text-[#05AF9E]  text-2xl">
               <img src={logo} alt="" />
             </div>
+            {/* Description */}
             <p className="mb-5 text-[#E0D2C3]">
               Ven y adentrate en la emocionante aventura al son de la guitarra
               payando y el sapucay de un gaucho de fondo
             </p>
             <span>Jugadores en linea: 0</span>
+            {/* Modal message */}
+            <div className="mt-10">
+              {/* Open the modal using document.getElementById('ID').showModal() method */}
+              <button
+                className="btn"
+                onClick={() =>
+                  document.getElementById("my_modal_1").showModal()
+                }
+              >
+                open modal
+              </button>
+              <dialog id="my_modal_1" className="modal">
+                <div className="modal-box">
+                  <h3 className="font-bold text-lg">Hello!</h3>
+                  <p className="py-4">
+                    Press ESC key or click the button below to close
+                  </p>
+                  <div className="modal-action">
+                    <form method="dialog">
+                      {/* if there is a button in form, it will close the modal */}
+                      <button className="btn">Close</button>
+                    </form>
+                  </div>
+                </div>
+              </dialog>
+            </div>
           </div>
         </div>
       </div>
+      {/* Hero end */}
       <div className="h-full">
         <div
           className="flex flex-col justify-center items-center h-full w-full"
@@ -39,6 +69,7 @@ function Home() {
             backgroundRepeat: "repeat",
           }}
         >
+          {/* Reglas init */}
           <p className="text-2xl text-blue-400">REGLAS</p>
           <p className="flex flex-col text-xl">
             1-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
@@ -54,6 +85,7 @@ function Home() {
           </p>
         </div>
       </div>
+      {/* Footer */}
       <div className="w-full">
         <Footer />
       </div>
--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -12,6 +12,7 @@ function Players() {
   const [data, setData] = useState(undefined);
   const [currentPlayer, setCurrentPlayer] = useState(undefined);
 
+  /* Trae los datos de la base */
   useEffect(() => {
     const querydb = getFirestore();
     const queryCollection = collection(querydb, "players");
@@ -23,62 +24,92 @@ function Players() {
   }, []);
 
   return (
-    <div className="w-full h-full flex justify-center items-center flex-col gap-40 bg-gradient-to-b from-[#2D3142] to-[#4F5D75]">
-      <div className="w-1/3 h-12 bg-[#BFC0C0] rounded-md pagination">
-        <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={5}
-          slidesPerView={4}
-          loop={true}
-          pagination={{ clickable: true, el: ".custom-pagination" }}
-        >
-          {data ? (
-            data.map((player) => {
-              return (
-                <SwiperSlide key={player.id}>
-                  <button
-                    onClick={() =>
-                      setCurrentPlayer({
-                        id: player.id,
-                        logo: player.logo,
-                        skin: player.skin,
-                      })
-                    }
-                  >
-                    <div className="tooltip tooltip-right" data-tip={player.id}>
-                      <img src={player.logo} alt={player.id} className="h-12" />
-                    </div>
-                  </button>
-                </SwiperSlide>
-              );
-            })
-          ) : (
-            <p className="flex justify-center w-full">A</p>
-          )}
-        </Swiper>
-        <div className="custom-pagination  flex justify-center"></div>
+    <div className="w-full h-full flex justify-evenly items-center bg-gradient-to-b from-[#2D3142] to-[#4F5D75]">
+      <div className="w-1/6 h-2/3 ml-10 rounded-lg bg-[#2D3142]">
+        <p className="text-2xl flex justify-center">Donaciones</p>
+        <ul className="mt-10 list-disc ml-5">
+          {data
+            ? [...data] // Crea una copia para no mutar el original
+                .sort((a, b) => b.donation - a.donation)
+                .map((player) => {
+                  return (
+                    <li key={player.id}>{player.id + " " + player.donation}</li>
+                  );
+                })
+            : false}
+        </ul>
       </div>
-      <div className="shadow-2xl drop-shadow-sm w-1/2 h-1/3 rounded-lg bg-[#2D3142]">
-        {currentPlayer ? (
-          <div className="flex h-full w-full justify-end pt-5">
-            <div className="text-2xl text-[#EF8354]  w-1/4 pl-5">
-              <p>{currentPlayer.id}</p>
+      {/* Swiper */}
+      <div className="flex flex-col justify-center items-center gap-24 w-full h-full">
+        <div className="w-1/3 h-12 bg-[#BFC0C0] rounded-md pagination">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={5}
+            slidesPerView={4}
+            loop={true}
+            pagination={{ clickable: true, el: ".custom-pagination" }}
+          >
+            {data ? (
+              data.map((player) => {
+                return (
+                  <SwiperSlide key={player.id}>
+                    <button
+                      onClick={() =>
+                        setCurrentPlayer({
+                          id: player.id,
+                          logo: player.logo,
+                          skin: player.skin,
+                        })
+                      }
+                    >
+                      <div
+                        className="tooltip tooltip-right z-40"
+                        data-tip={player.id}
+                      >
+                        <img
+                          src={player.logo}
+                          alt={player.id}
+                          className="h-12 z-30 rounded-md"
+                        />
+                      </div>
+                    </button>
+                  </SwiperSlide>
+                );
+              })
+            ) : (
+              <p className="flex justify-center w-full">A</p>
+            )}
+          </Swiper>
+          <div className="custom-pagination  flex justify-center"></div>
+        </div>
+        {/* Swiper END */}
+
+        {/* Players information */}
+        <div className="shadow-2xl drop-shadow-sm w-11/12 h-1/3 rounded-lg bg-[#2D3142]">
+          {currentPlayer ? (
+            <div className="flex h-full w-full justify-end pt-5">
+              {/* Username */}
+              <div className="text-2xl text-[#EF8354]  w-1/4 pl-5">
+                <p>{currentPlayer.id}</p>
+              </div>
+              <div className=" flex justify-center items-center w-1/2 ml-5">
+                {/* Player information */}
+                <p className="mb-5">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Perferendis veritatis mollitia blanditiis maiores sit
+                  accusamus laborum doloribus itaque hic atque odio, tenetur
+                  ratione illo illum, eius quos qui neque cumque!
+                </p>
+              </div>
+              {/* Player skin image */}
+              <div className=" flex flex-col-reverse w-1/2">
+                <img src={currentPlayer.skin} alt="" className="h-80" />
+              </div>
             </div>
-            <div className=" flex justify-center items-center w-1/2 ml-5">
-              <p className="mb-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Perferendis veritatis mollitia blanditiis maiores sit accusamus
-                laborum doloribus itaque hic atque odio, tenetur ratione illo
-                illum, eius quos qui neque cumque!
-              </p>
-            </div>
-            <div className=" flex flex-col-reverse w-1/2">
-              <img src={currentPlayer.skin} alt="" className="h-64" />
-            </div>
-          </div>
-        ) : (
-          false
-        )}
+          ) : (
+            false
+          )}
+        </div>
       </div>
     </div>
   );
